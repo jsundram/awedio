9afc6238065041958c722797c30cce1365a8fec1
Ryan Berdeen <ryan@ryanberdeen.com>
2010-05-12 00:24:53 -0400

    working playback

--- awesome.js
+++ awesome.js
@@ -1,32 +1,48 @@
-var buffer = [];
 var samples;
 var count = 0;
 
+var audioOutput = new Audio();
+audioOutput.mozSetup(2, 44100, 1);
 
 function audioWritten(event)
 {
+    if (!audio.extractSamples) {
+        return;
+    }
+
+    audio = event.target;
     // sample data is obtained using samples.item(n)
     samples = event.mozFrameBuffer;
-    for (var i = 0; i < samples.length; i++)
-        buffer.push(samples.item(i));
-    
+    var start = audio.buffer.length;
+    var out = [];
+    for (var i = 0; i < samples.length; i++) {
+        var sample = samples.item(i);
+        //out.push(sample);
+        audio.buffer.push(sample);
+        if (audio.buffer.length > audio.extractSamples) {
+            audio.pause();
+            audio.callback(audio.buffer);
+            audio.callback = null;
+            audio.extractSamples = null;
+            break;
+        }
+    }
+
+    //audioOutput.mozWriteAudio(out.length, out);
+
     count += 1;
-    audio = document.getElementById("player");
-    
-    document.getElementById("debug").innerHTML += "Got audio: " + count + " samples=" + samples.length + "time= " + audio.currentTime + " buffer:" + buffer.length + "\n";
 }
 
 // offset and duration in seconds
 function get_audio(offset, duration, callback)
 {
     audio = document.getElementById("player");
-    buffer = [];
+    audio.volume = 0;
+    audio.buffer = [];
     audio.currentTime = offset; // seek
+    audio.callback = callback;
     
     var frames = duration * 44100 * 2;
-    while (buffer.length < frames)
-    {
-        
-    }
-    document.getElementById("debug").innerHTML =  "got buffer:" + buffer.length;
+    audio.extractSamples = frames;
+    audio.play();
 }
--- lookatthisfuckingaudio.js
+++ lookatthisfuckingaudio.js
@@ -1,5 +1,5 @@
 var ROWS = 4;
-var COLS = 8;
+var COLS = 16;
 
 function drawGrid() {
     var ctx = gridElt.getContext('2d');
@@ -8,19 +8,19 @@ function drawGrid() {
     ctx.strokeStyle = '#aaaaaa';
     // DRAW COLS
     for (var i = 0; i < COLS; i++) {
-        var x = (i + 1) * colWidth;
+        var x = Math.floor((i + 1) * colWidth) + .5;
         ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, gridElt.height);
+        ctx.moveTo(x, rowHeight / 2);
+        ctx.lineTo(x, gridElt.height - rowHeight / 2);
         ctx.stroke();
     }
 
     // DRAW ROWS
     for (var i = 0; i < ROWS; i++) {
-        var y = (i + 1) * rowHeight;
+        var y = Math.floor((i + 1) * rowHeight) + .5;
         ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(gridElt.width, y);
+        ctx.moveTo(colWidth / 2, y);
+        ctx.lineTo(gridElt.width - colWidth / 2, y);
         ctx.stroke();
     }
 
@@ -36,23 +36,25 @@ function drawGrid() {
 
     if (draggingGridCoord != null) {
         ctx.beginPath();
-        ctx.arc((draggingGridCoord[1] + 1) * colWidth, (draggingGridCoord[0] + 1) * rowHeight, 20, 0, Math.PI * 2, true);
+        ctx.arc((draggingGridCoord[1] + 1) * colWidth, (draggingGridCoord[0] + 1) * rowHeight, 14, 0, Math.PI * 2, true);
         ctx.fill();
     }
 }
 
 function drawTimeline() {
     var ctx = timelineElt.getContext('2d');
+    ctx.strokeStyle = '#aaaaaa';
+    ctx.clearRect(0, 0, timelineElt.width, timelineElt.height);
 
     for (var i = 0; i < ranges.length; i++) {
         var range = ranges[i];
-        ctx.strokeRect(range.start * scale + 2, 0, range.duration * scale - 4, timelineElt.height);
+        ctx.strokeRect(Math.floor(range.start * scale + 1) + .5, .5, Math.floor(range.duration * scale - 2), timelineElt.height - 1);
     }
 }
 
 function drawDraggingRange() {
     var ctx = draggingRangeElt.getContext('2d');
-    ctx.strokeRect(0, 0, draggingRange.duration * scale - 4, timelineElt.height);
+    ctx.fillRect(0, 0, Math.floor(draggingRange.duration * scale - 4), timelineElt.height);
 }
 
 var timelineElt;
@@ -67,6 +69,8 @@ var draggingGridCoord = null;
 
 var gridData;
 
+var columnSamples = 44100 * 2 * .5;
+
 function init() {
     gridElt = $('grid');
     timelineElt = $('timeline');
@@ -79,9 +83,14 @@ function init() {
 
     clearGridData();
 
+    gridElt.observe('click', handleGridClick);
+
     timelineElt.observe('mousedown', handleTimelineMousedown);
-    drawTimeline();
+    $('range_type').observe('change', updateTimeline);
+    updateTimeline();
     drawGrid();
+
+    $('play_button').observe('click', playGrid);
 }
 
 function clearGridData() {
@@ -94,6 +103,11 @@ function clearGridData() {
     }
 }
 
+function updateTimeline() {
+    ranges = analysis[$('range_type').value];
+    drawTimeline();
+}
+
 function handleTimelineMousedown(e) {
     var x = e.pointerX();
     var position = (e.pointerX() - timelineElt.cumulativeOffset().left + timelineElt.cumulativeScrollOffset().left) / scale;
@@ -101,15 +115,16 @@ function handleTimelineMousedown(e) {
         var range = ranges[i];
         if (position >= range.start && position < range.start + range.duration) {
             draggingRange = range;
+            get_audio(range.start, range.duration, function(audio) {range.audio = audio});
             draggingRangeElt.show();
             draggingRangeElt.width = Math.ceil(range.duration * scale);
             drawDraggingRange();
+            $(document).observe('mousemove', handleDragMousemove);
+            $(document).observe('mouseup', handleDragMouseup);
+
+            positionDraggingRange(e.pointerX(), e.pointerY());
+            break;
         }
-
-        $(document).observe('mousemove', handleDragMousemove);
-        $(document).observe('mouseup', handleDragMouseup);
-
-        positionDraggingRange(e.pointerX(), e.pointerY());
     }
     e.stop();
 }
@@ -135,6 +150,21 @@ function positionDraggingRange(x, y) {
     }
 }
 
+function handleGridClick(e) {
+    e.stop();
+    var gridPos = gridElt.cumulativeOffset();
+    var gridX = e.pointerX() - gridPos.left;
+    var gridY = e.pointerY() - gridPos.top;
+    var coord = getCoord(gridX, gridY);
+    for (var i = coord[1] - 1; i >= 0; i--) {
+        if (gridData[i][coord[0]]) {
+            gridData[coord[1]][coord[0]] = gridData[i][coord[0]];
+            drawGrid();
+            break;
+        }
+    }
+}
+
 function getCoord(x, y) {
     var col = Math.floor((x + colWidth / 2) / colWidth) - 1;
     var row = Math.floor((y + rowHeight / 2) / rowHeight) - 1;
@@ -157,4 +187,34 @@ function handleDragMouseup(e) {
     $(document).stopObserving('mousemove', handleDragMousemove);
     $(document).stopObserving('mouseup', handleDragMouseup);
     e.stop();
+}
+
+function getColumnAudio(columnIndex) {
+    var result = [];
+
+    var column = gridData[columnIndex];
+
+    var numSamples = 0;
+
+    for (var i = 0; i < columnSamples; i++) {
+        var sample = 0;
+        for (var j = 0; j < ROWS; j++) {
+            if (column[j] && column[j].audio.length > i) {
+                sample += column[j].audio[i];
+                numSamples++;
+            }
+        }
+        result[i] = sample;
+    }
+    return result;
+}
+
+function playGrid() {
+    var audioOutput = new Audio();
+    audioOutput.mozSetup(2, 44100, 1);
+
+    for (var i = 0; i < COLS; i++) {
+        var samples = getColumnAudio(i);
+        var buffered = audioOutput.mozWriteAudio(samples.length, samples);
+    }
 }
