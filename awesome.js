f25db1113a087f5b5c036ef8922aaf63178ab819
Ryan Berdeen <ryan@ryanberdeen.com>
2010-05-12 10:59:24 -0400

    unroll loop for stereo

--- awesome.js
+++ awesome.js
@@ -13,22 +13,25 @@ function audioWritten(event)
     audio = event.target;
     // sample data is obtained using samples.item(n)
     samples = event.mozFrameBuffer;
-    var start = audio.buffer.length;
     var out = [];
-    for (var i = 0; i < samples.length; i++) {
-        var sample = samples.item(i);
-        //out.push(sample);
-        audio.buffer.push(sample);
-        if (audio.buffer.length > audio.extractSamples) {
+    for (var i = 0; i < samples.length; i+=2) {
+        if (audio.buffer.length == audio.extractSamples) {
             audio.pause();
             audio.callback(audio.buffer);
             audio.callback = null;
             audio.extractSamples = null;
+            audio.buffer = null;
             break;
         }
+        var sample = samples.item(i);
+        out.push(sample);
+        audio.buffer.push(sample);
+        var sample = samples.item(i+1);
+        out.push(sample);
+        audio.buffer.push(sample);
     }
 
-    //audioOutput.mozWriteAudio(out.length, out);
+    audioOutput.mozWriteAudio(out.length, out);
 
     count += 1;
 }
@@ -41,8 +44,9 @@ function get_audio(offset, duration, callback)
     audio.buffer = [];
     audio.currentTime = offset; // seek
     audio.callback = callback;
-    
-    var frames = duration * 44100 * 2;
+
+    // TODO sample accuracy?
+    var frames = Math.floor(duration * 44100) * 2;
     audio.extractSamples = frames;
     audio.play();
 }
